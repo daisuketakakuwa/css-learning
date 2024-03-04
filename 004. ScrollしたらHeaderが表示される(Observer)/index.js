4871fd507e62024ed29641864d6212b3d7127a9a
dtakakuwa129 <daisuke.takakuwa129@gmail.com>
2024-03-04 16:44:32 +0900

    004. ScrollしたらHeaderが表示される(Observer)

--- 004. ScrollしたらHeaderが表示される(Observer)/index.js
+++ 004. ScrollしたらHeaderが表示される(Observer)/index.js
@@ -1,38 +1,39 @@
 // Target要素は縦横は100vh × 100vh なので、
 // 文字のラインのところにきたらintersectする。
 // 実行は１回だけでOKなので、実行後監視対象外とする。
-const title1 = document.querySelector(".c1");
-const title2 = document.querySelector(".c2");
-const title3 = document.querySelector(".c3");
+const kvArea = document.querySelector(".kv-area");
 
-const cb = (entries, observer) => {
+const cb = (entries) => {
+  const headerMenuElem = document.querySelector(".header-menu");
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      // ID(.c1/.c2/.c3)取得
-      const keyClass = entry.target.classList[1];
-      // 文字列をすべて取得 & Animation付与
-      const elements = document.querySelectorAll(`.${keyClass} > .letter`);
-      elements.forEach((elem) => {
-        elem.classList.add("drop-letter");
-      });
+        headerMenuElem.classList.remove("show-header");
+    } else {
+        // HeaderMenuを表示する
+
+        // CSSの場合
+        // 1. opacity: 1のクラスを付与する      
+        headerMenuElem.classList.add("show-header");
+
+        // Reactの場合
+        // 1. useStateで状態を更新
+        // 2. opacityを1へ変更する
     }
+    // NOT intersectで、HeaderMenuを表示する
   });
 };
 
 const options = {
   root: null,
   // ある程度タイトルが見える位置を intersect 位置とする。
-  rootMargin: "-50%",
+  rootMargin: "0px",
 };
 
 // 1. 監視対象 title
 // 2. アクション cb
 // 3. 1 を監視　isIntersecting　になったら　2(cb)を実行させる。
 const io = new IntersectionObserver(cb, options);
-io.observe(title1);
-io.observe(title2);
-io.observe(title3);
-
+io.observe(kvArea);
 
 // 1. KV画像エリアが完全にスクロールで消えたら、
 // 2. ぬるっとHeaderMenu部分を表示させる。
